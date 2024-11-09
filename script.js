a6ca3e619f15e8a2fc2c6578ec524010dd336907
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-11-09 21:27:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
   // Fix Navbar Position
   document.querySelector('#content')?.prepend(document.querySelector('.t-navbar') ?? '')
+
+  // Switch language button
+  const tSwitchLang = refinedCppConfig.currentLang === 'zh' ? 'English' : '中文'
+  const btnSwitch = document.querySelector('#ca-nstab-main > span')
+  btnSwitch.innerHTML = `<div style="
+      font-size: 12px;
+      cursor: pointer;
+  ">${tSwitchLang}</div>`
+  let preload
+  
+  btnSwitch.onclick = ()=>{
+      document.cookie = `prefer-lang=${refinedCppConfig.currentLang === 'zh' ? 'en' : 'zh'}`
+      document.location.reload()
+  }
 })
