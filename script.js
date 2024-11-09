fcad344574f490336df23ae6d8dc5a1d18f8a8a1
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-11-09 21:09:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+  // Header
   const header = document.querySelector('#cpp-head-first');
-  header.style.cursor = 'pointer'
-  header.onclick = ()=>document.location.href = '/'
+  header.style.cursor = 'pointer';
+  header.onclick = ()=>document.location.href = '/';
+
+  // Fix Navbar Position
+  document.querySelector('#content')?.prepend(document.querySelector('.t-navbar'))
 })
