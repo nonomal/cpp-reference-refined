18c59a152cce81505cd07cb0be06cdb3331ee073
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-11-09 21:07:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('load', ()=>{
+document.addEventListener('DOMContentLoaded', ()=>{
   const header = document.querySelector('#cpp-head-first');
   header.style.cursor = 'pointer'
   header.onclick = ()=>document.location.href = '/'
