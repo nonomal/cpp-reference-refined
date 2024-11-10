a1d85c204b802e36fc4b4a40701841e82bed2cfb
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2024-11-10 13:48:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,16 +8,32 @@ document.addEventListener('DOMContentLoaded', ()=>{
   document.querySelector('#content')?.prepend(document.querySelector('.t-navbar') ?? '')
 
   // Switch language button
-  const tSwitchLang = refinedCppConfig.currentLang === 'zh' ? 'English' : '中文'
+  const langContentCache = {}
+  const runLanguagePreload = async (languageToPreload = (refinedCppConfig.currentLang === 'en' ? 'zh' : 'en')) => {
+      const elem = document.createElement('div')
+      elem.innerHTML = await fetch('?lang='+languageToPreload).then(v=>v.text())
+      langContentCache[languageToPreload] = elem.querySelector('#content').innerHTML
+  }
+  runLanguagePreload();
+  
+  const contentDom = document.querySelector('#content')
+  langContentCache[refinedCppConfig.currentLang] = contentDom.innerHTML
+  
+  const tSwitchLang = ()=>refinedCppConfig.currentLang === 'zh' ? 'English' : '中文'
   const btnSwitch = document.querySelector('#ca-nstab-main > span')
   btnSwitch.innerHTML = `<div style="
       font-size: 12px;
       cursor: pointer;
-  ">${tSwitchLang}</div>`
-  let preload
+  " class="btnSwitchLang">${tSwitchLang()}</div>`
   
-  btnSwitch.onclick = ()=>{
-      document.cookie = `prefer-lang=${refinedCppConfig.currentLang === 'zh' ? 'en' : 'zh'}`
-      document.location.reload()
+  btnSwitch.onclick = () => {
+      const targetLang = refinedCppConfig.currentLang === 'zh' ? 'en' : 'zh';
+      document.cookie = `prefer-lang=${targetLang}`
+      if (langContentCache[targetLang]) {
+        refinedCppConfig.currentLang = targetLang
+        contentDom.innerHTML = langContentCache[targetLang]
+        document.querySelector('.btnSwitchLang').innerText = tSwitchLang();
+      } else
+        document.location.reload()
   }
 })
